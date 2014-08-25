2a290cc11af0fd7c8964d66c678db62e46f150e9
kyokomi <kyoko1220adword@gmail.com>
2014-08-25 22:29:57 +0900

    update v0.2.1

--- quick-image-cli.rb
+++ quick-image-cli.rb
@@ -6,8 +6,8 @@ require "formula"
 
 class QuickImageCli < Formula
   homepage ""
-  url "https://github.com/kyokomi/quick-image-cli/releases/download/v0.2.0/quick-image-cli.zip"
-  sha1 "965cdf7973a7215da34920000b750e35c3e69b4a"
+  url "https://github.com/kyokomi/quick-image-cli/releases/download/v0.2.1/quick-image-cli.zip"
+  sha1 "ff9503e7c0e3fdd7e4b4275306806eec62b4df35"
 
   # depends_on "cmake" => :build
   # depends_on :x11 # if your formula requires any X11/XQuartz components
